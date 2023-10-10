e7c4e54387805d8c437b947b4fe063e300dab8cb
DevWithEasy <devwitheasy@gmail.com>
2023-10-10 12:55:35 +0600

    authentication

--- server/src/middleware/authentication.ts
+++ server/src/middleware/authentication.ts
@@ -3,26 +3,27 @@ import * as jwt from 'jsonwebtoken'
 import AppError from "../utils/AppError";
 
 interface CustomRequest extends Request {
-    user : string;
+    user: string;
 }
 
-const authenticated =async (req : CustomRequest,res : Response,next: NextFunction)=>{
+const authenticated = async (req: CustomRequest, res: Response, next: NextFunction) => {
     try {
-        const token  = req.headers['authorization'].split(' ')[1]
+        const token = req.headers['authorization'].split(' ')[1]
         if (!token) {
             return next(new AppError(401, 'Authentication failed.'));
-          }
+        }
 
-        jwt.verify(token,process.env.JWT_SECRET,(err,decode)=>{
-            if (err){
-                next(new AppError(401,'Authentication failed.'))
-            }else{
-                const decodeUser = decode as {id : string}
+        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+            if (err) {
+                next(new AppError(401, 'Authentication failed.'))
+            } else {
+                const decodeUser = decode as { id: string }
                 req.user = decodeUser.id
+                next()
             }
         })
 
     } catch (error) {
-        next(new AppError(500,'Internal Server Error'))
+        next(new AppError(500, 'Internal Server Error'))
     }
 }
