139882873474b92d2c92b5afe3412a0461da0801
DevWithEasy <devwitheasy@gmail.com>
2023-10-14 16:04:33 +0600

    react multi carouel add

--- client/src/components/home/CategorySlide.tsx
+++ client/src/components/home/CategorySlide.tsx
@@ -1,53 +1,5 @@
-import Slider from "react-slick";
-import { productData } from "../../assets/productdata";
-
 const CategorySlide = () => {
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-  return (
-    <div>
-      <Slider {...settings}>
-        {productData.map((data) => {
-          <div key={data.id} className="border">
-            {/* <img src={data.image} /> */}
-            <h2>{data.name}</h2>
-          </div>;
-        })}
-      </Slider>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default CategorySlide;
--- client/src/components/home/HomeSlider.tsx
+++ client/src/components/home/HomeSlider.tsx
@@ -1,26 +1,15 @@
-import Slider from "react-slick";
 import img1 from "../../assets/image/header_image_01.webp";
 import img2 from "../../assets/image/header_image_02.webp";
 
 const HomeSlider = () => {
-  const settings = {
-    dots: true, 
-    infinite: true, 
-    speed: 500,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
   return (
     <div className="p-2 overflow-hidden">
-      <Slider {...settings}>
-        <div>
-          <img src={img1} />
-        </div>
-        <div>
-          <img src={img2} />
-        </div>
-      </Slider>
+      <div>
+        <img src={img1} />
+      </div>
+      <div>
+        <img src={img2} />
+      </div>
     </div>
   );
 };
